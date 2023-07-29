f92f14928c4fefea7920abced14dbcca6b5ffe56
Peder Goodman <peder.goodman@gmail.com>
2023-07-28 20:09:29 -0500

    add dummy data SQL to database

--- database.sql
+++ database.sql
@@ -42,9 +42,6 @@ CREATE TABLE "question_categories" (
 	);
 	
 
-
-
-
 CREATE TABLE "user_favorited" (
 	"id" serial NOT NULL PRIMARY KEY,
 	"user_id" integer NOT NULL REFERENCES "user",
@@ -52,7 +49,70 @@ CREATE TABLE "user_favorited" (
 	);
 
 
+-- dummy data for initial stuff!
 
+-- categories
+INSERT INTO "category" ("category_name")
+VALUES 
+('Self-Discovery'),
+('Philosophy'),
+('Reflection'),
+('Creativity'),
+('Ethics'),
+('Environment'),
+('Relationships'),
+('Aspirations'),
+('Society');
+
+
+
+-- default test questions!
+INSERT INTO "questions" ("question")
+VALUES 
+  -- Self-Discovery
+  ('What are your core values, and how do they influence your decisions?'),
+  ('Describe a life-changing experience that shaped who you are today.'),
+  ('What are your biggest fears, and how do they hold you back?'),
+  
+  -- Philosophy
+  ('What is the meaning of life to you?'),
+  ('Do you believe in fate or free will? Why?'),
+  ('How would you define true happiness and contentment?'),
+  
+  -- Reflection
+  ('What are some areas in your life that you want to improve, and how do you plan to achieve that?'),
+  ('Describe a recent mistake you made and what you learned from it.'),
+  ('How do you handle and overcome challenges in life?'),
+
+  -- Creativity
+  ('If you could have any superpower, what would it be, and how would you use it?'),
+  ('Write about a fictional world you would love to live in and its unique features.'),
+  ('How do you express your creativity, and what inspires you?'),
+  
+  -- Ethics
+  ('Is there a situation where telling a small lie is justified? Why or why not?'),
+  ('Discuss a moral dilemma you faced and how you resolved it.'),
+  ('How do you determine what is right and wrong in your life?'),
+
+  -- Environment
+  ('What is your favorite natural place, and why does it hold significance for you?'),
+  ('How do you connect with nature on a regular basis?'),
+  ('Discuss the impact of human activities on the environment and what you can do to make a positive change.'),
+  
+  -- Relationships
+  ('Describe a moment when you felt truly understood by someone else.'),
+  ('How do you show empathy towards others, and why is it essential?'),
+  ('Discuss the qualities that make a relationship strong and fulfilling.'),
+  
+  -- Aspirations
+  ('What are your long-term goals, and what steps are you taking to achieve them?'),
+  ('Write about a dream you have that seems impossible, but you wish to pursue anyway.'),
+  ('How do you stay motivated and focused on your objectives?'),
+  
+  -- Society
+  ('What are some potential benefits and drawbacks of advancing technology in our lives?'),
+  ('How do you strike a balance between using technology for productivity and avoiding its negative effects?'),
+  ('Discuss the role of social media in modern society and its impact on relationships and mental health.');
 
 
 
