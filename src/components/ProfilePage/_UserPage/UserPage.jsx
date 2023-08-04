72a9039874124be611de5f95591e4d10bf9f80be
Peder Goodman <peder.goodman@gmail.com>
2023-08-04 10:21:31 -0500

    rough styled ProfilePage, no data, but looks good
    broguht in JournalContainer & FavoritesContainer

---
+++ src/components/ProfilePage/_UserPage/UserPage.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import LogOutButton from "../../LogOutButton/LogOutButton";
+import { useSelector } from "react-redux";
+
+// MUI IMPORTS
+import { Box } from "@mui/material";
+
+// CUSTOM COMPONENTS
+import JournalContainer from "../JournalContainer/JournalContainer";
+import FavoritesContainer from "../FavoritesContainer/FavoritesContainer";
+
+function UserPage() {
+  // this component doesn't do much to start, just renders some user reducer info to the DOM
+  const user = useSelector(store => store.user);
+
+  const viewContainerStyle = {
+    display: "flex",
+    justifyContent: "center",
+    padding: "0 16px",
+  };
+
+  const favoritesContainerStyle = {
+    padding: '10px',
+    margin: '0 5px',
+    backgroundColor: 'red',
+    maxWidth: '45%',
+    minWidth: '40%',
+    width: '45%',
+    maxHeight: '675px'
+  };
+
+  const journalContainerStyle = {
+    backgroundColor: 'cadetblue',
+    padding: '10px',
+    maxWidth: '45%',
+    width: '100%',
+    maxHeight: '675px'
+  };
+
+  return (
+    <>
+      {/* <div className="container">
+        <h2>Welcome, {user.username}!</h2>
+        <p>Your ID is: {user.id}</p>
+        <LogOutButton className="btn" />
+      </div> */}
+      <Box sx={viewContainerStyle}>
+        <Box sx={favoritesContainerStyle}>
+          <FavoritesContainer />
+        </Box>
+        <Box sx={journalContainerStyle}>
+          <JournalContainer />
+        </Box>
+      </Box>
+    </>
+  );
+}
+
+// this allows us to use <App /> in index.js
+export default UserPage;
