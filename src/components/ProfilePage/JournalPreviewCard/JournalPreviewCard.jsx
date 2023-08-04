eab6c53baf35f0ed3f8157fdfa56fd05d22da885
Peder Goodman <peder.goodman@gmail.com>
2023-08-04 10:20:22 -0500

    add JournalPreviewCard

---
+++ src/components/ProfilePage/JournalPreviewCard/JournalPreviewCard.jsx
@@ -0,0 +1,60 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import { CardActionArea } from "@mui/material";
+
+
+const bull = (
+  <Box
+    component="span"
+    sx={{ display: "inline-block", mx: "2px", transform: "scale(0.8)" }}
+  >
+    •
+  </Box>
+);
+
+const card = (
+  <React.Fragment>
+          <CardActionArea>
+    <CardContent>
+      <Typography
+        sx={{ fontSize: 14 }}
+        color="text.secondary"
+        gutterBottom
+      ></Typography>
+      <Typography variant="h5" component="div">
+        be{bull}nev{bull}o{bull}lent
+      </Typography>
+      <Typography sx={{ mb: 1.5 }} color="text.secondary">
+        adjective
+      </Typography>
+      <Typography variant="body2">
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book. It has survived not only five
+        centuries, but also the leap into electronic typesetting, remaining
+        essentially unchanged. It was popularised in the 1960s with the release
+        of Letraset sheets containing Lorem Ipsum passages, and more recently
+        with desktop publishing software like Aldus PageMaker including versions
+        of Lorem Ipsum.
+      </Typography>
+    </CardContent>
+    <CardActions>
+      <Button size="small">Learn More</Button>
+    </CardActions>
+    </CardActionArea>
+  </React.Fragment>
+);
+
+export default function JournalPreviewCard() {
+  return (
+    <Box sx={{ minWidth: 275, margin: '5px', margin: '8px 0' }}>
+      <Card variant="outlined">{card}</Card>
+    </Box>
+  );
+}
