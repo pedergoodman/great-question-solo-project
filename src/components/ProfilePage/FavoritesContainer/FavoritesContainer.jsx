69e848ac400512b22896e4b411a72d5e8160535b
Peder Goodman <peder.goodman@gmail.com>
2023-08-05 19:03:34 -0500

    profile, favorites container rough style, needs data

--- src/components/ProfilePage/FavoritesContainer/FavoritesContainer.jsx
+++ src/components/ProfilePage/FavoritesContainer/FavoritesContainer.jsx
@@ -1,28 +1,46 @@
-import * as React from "react";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+
+// MUI COMPONENTS
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import ListItemText from "@mui/material/ListItemText";
 import ListSubheader from "@mui/material/ListSubheader";
-
-import ExpandLess from "@mui/icons-material/ExpandLess";
-import ExpandMore from "@mui/icons-material/ExpandMore";
 import { Box, Collapse, IconButton, Typography } from "@mui/material";
-import { useState } from "react";
+
+// MUI ICONS
+import ExpandMore from "@mui/icons-material/ExpandMore";
+import { ChevronRight } from "@mui/icons-material";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import MenuBookIcon from "@mui/icons-material/MenuBook";
 
 export default function JournalContainer() {
+  const history = useHistory();
+  const dispatch = useDispatch();
+
   const [open, setOpen] = useState(true);
+  const [fav, setFav] = useState(false);
+
+  // need to replace this with favories but the full list is nice so i have more data to view
+  const questionsList = useSelector(store => store.questions);
+  console.log("in profile questionsList is:", questionsList);
 
   const handleClick = () => {
     setOpen(!open);
   };
 
-  // TEMP ARRAY DATA
+  // grabs question data and fill bubbles!
+  useEffect(() => {
+    dispatch({
+      type: "FETCH_QUESTIONS",
+    });
+  }, []);
 
-//   width: '100%',
-// maxHeight: '675px',
-// backgroundColor: '#fff',
-// position: 'relative',
-// overflow: 'auto'
+  const startJournal = () => {
+    console.log("clicked start journal!");
+  };
 
   return (
     <List
@@ -31,35 +49,53 @@ export default function JournalContainer() {
         bgcolor: "background.paper",
         position: "relative",
         overflow: "auto",
-        maxHeight: '675px',
+        maxHeight: "675px",
         paddingBottom: 0,
         "& ul": { padding: 0 },
       }}
       subheader={<li />}
     >
       {/* maping the header */}
-      {[0, 1, 2, 3, 4].map((sectionId) => (
+      {[0, 1, 2, 3, 4, 5].map(sectionId => (
         <li key={`section-${sectionId}`}>
+          
+          
           <ul>
             <ListSubheader sx={{ display: "flex", alignItems: "center" }}>
-              <Typography>{`I'm sticky ${sectionId}`}</Typography>
               <IconButton
                 aria-label="fingerprint"
                 color="secondary"
                 onClick={handleClick}
               >
-                {open ? <ExpandLess /> : <ExpandMore />}
+                {open ? <ExpandMore /> : <ChevronRight />}
               </IconButton>
+              <Typography>{`I'm sticky ${sectionId}`}</Typography>
             </ListSubheader>
             <Collapse in={open} timeout="auto" unmountOnExit>
               {/* maping the sub headers as unordered list */}
-              {[0, 1, 2].map(item => (
+              {[0, 1, 2, 3, 4].map(item => (
                 <ListItem key={`item-${sectionId}-${item}`}>
                   <ListItemText primary={`Item ${item}`} />
+                  <IconButton
+                    aria-label="fingerprint"
+                    color="secondary"
+                    onClick={() => setFav(!fav)}
+                  >
+                    {fav ? <FavoriteIcon /> : <FavoriteBorderIcon />}
+                  </IconButton>
+                  <IconButton
+                    aria-label="fingerprint"
+                    color="secondary"
+                    onClick={startJournal}
+                  >
+                    <MenuBookIcon />
+                  </IconButton>
                 </ListItem>
               ))}
             </Collapse>
           </ul>
+
+          
         </li>
       ))}
     </List>
