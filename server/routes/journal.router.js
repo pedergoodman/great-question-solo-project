88a7842adb2795457f3293db8de761810887374c
Peder Goodman <peder.goodman@gmail.com>
2023-08-06 16:52:19 -0500

    add journal get route

--- server/routes/journal.router.js
+++ server/routes/journal.router.js
@@ -2,26 +2,42 @@ const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
 
-/**
- * GET route template
- */
-router.get('/', (req, res) => {
+// GET user journals
+router.get('/:id', (req, res) => {
 
-  // grab items by category with all the sub questions
-  const sqlQuery = `
+  const userId = req.params.id;
+  // const userId = req?.user?.id;
 
+  const sqlText = `
+    SELECT 
+      journals.id AS "journalId",
+      journals.title AS "journalTitle",
+      journals.journal_entry AS "JournalBody",
+      journals.created_date AS "CreatedDate",
+      journals.edited_date AS "EditedDate",
+      journals.question_id AS "questionId",
+      questions.question AS "questionText",
+      categories.id AS "categoryId",
+      categories.category_name AS "categoryName"
+    FROM journals 
+    JOIN questions
+      ON journals.question_id = questions.id
+    JOIN question_categories
+      ON questions.id = question_categories.question_id
+    JOIN categories
+      ON question_categories.category_id = categories.id
+    WHERE 
+      journals.user_id = $1
   ;`
-  // delete this later
-  res.sendStatus(200)
-  
-  // pool.query(sqlQuery)
-  //   .then((result) => {
 
-  //     res.send(result.rows)
-  //   }).catch((err) => {
-  //     console.log('error GETing journals from server', err);
-  //     res.sendStatus(500)
-  //   });
+  pool.query(sqlText, [userId])
+    .then((result) => {
+      console.log(result.rows);
+      res.send(result.rows)
+    }).catch((err) => {
+      console.log('error ADDING favorite to database', err);
+      res.sendStatus(500)
+    });
 
 
 
