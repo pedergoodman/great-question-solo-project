b8cb44cbe8bf3a787218ea40fb292e10c4700bb0
Peder Goodman <peder.goodman@gmail.com>
2023-08-07 14:34:21 -0500

    combined questions reducers
    moved favoires into question.reducer
    added custom into question.reducer

--- src/redux/reducers/_root.reducer.js
+++ src/redux/reducers/_root.reducer.js
@@ -2,8 +2,7 @@ import { combineReducers } from 'redux';
 import errors from './errors.reducer';
 import user from './user.reducer';
 import journals from './journal.reducer'
-import allQuestions from './question.reducer';
-import favoriteQuestions from './favorite.reducer';
+import questions from './question.reducer';
 
 // rootReducer is the primary reducer for our entire project
 // It bundles up all of the other reducers so our project can use them.
@@ -15,8 +14,7 @@ const rootReducer = combineReducers({
   errors, // contains registrationMessage and loginMessage
   user, // will have an id and username if someone is logged in
   journals, // contains all journals of a logged in user & a store of an active journal edit
-  allQuestions, // contains a list of all the questions a user has access to
-  favoriteQuestions, // contains a list of user favorite questions by category
+  questions // contains a list of all the questions a user has access to: all, favorites, custom
 });
 
 export default rootReducer;
--- src/redux/reducers/question.reducer.js
+++ src/redux/reducers/question.reducer.js
@@ -1,3 +1,5 @@
+import { combineReducers } from 'redux';
+
 const allQuestions = (state = [], action) => {
 
   switch (action.type) {
@@ -10,6 +12,29 @@ const allQuestions = (state = [], action) => {
 
 
 
+const favoriteQuestions = (state = [], action) => {
+  switch (action.type) {
+    case 'SET_FAVORITES_LIST':
+      return action.payload;
+    default:
+      return state;
+  }
+};
+
+const customQuestions = (state = [], action) => {
+  switch (action.type) {
+    case 'SET_CUSTOM_QUESTIONS_LIST':
+      return action.payload;
+    default:
+      return state;
+  }
+};
+
+
 // questions will be on the redux state at:
-// state.questions
-export default allQuestions;
+// state.questions.<w/e question you want to accesss>
+export default combineReducers({
+  allQuestions,
+  favoriteQuestions,
+  customQuestions
+});
