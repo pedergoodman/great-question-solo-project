60d299e6e49b6c495a410a38cd5bb7b02b2ede6b
Peder Goodman <peder.goodman@gmail.com>
2023-08-06 19:39:05 -0500

    createCategoryListItem to fav list and custom list

--- src/redux/sagas/question.saga.js
+++ src/redux/sagas/question.saga.js
@@ -1,7 +1,7 @@
 import { put, takeLatest } from 'redux-saga/effects';
 import axios from 'axios';
 import { useSelector } from 'react-redux';
-
+import { createQuestionList, createCategoryListItem } from "../../utils/utils";
 
 function* fetchQuestions(action) {
   // const userId = action.payload
@@ -10,7 +10,7 @@ function* fetchQuestions(action) {
     // allQuestions comes in as an array of category objects
     const allQuestionsList = yield axios.get(`/api/questions/all`)
     // favoriteQuestions comes in as an array of category objects 
-    const favoriteQuestionsList = yield axios.get(`/api/questions/favorites`)
+    const favoriteQuestionsList = yield axios.get(`/api/favorite`)
     // customQuestions comes in as an array of question objects
     const customQuestionsList = yield axios.get(`/api/questions/custom`)
 
@@ -18,18 +18,23 @@ function* fetchQuestions(action) {
     console.log('allQuestionsList data is:', allQuestionsList.data);
     console.log('favoriteQuestionsList data is:', favoriteQuestionsList.data);
     console.log('customQuestionsList data is:', customQuestionsList.data);
-    // merge these things here??? 
 
 
     // TODO - convert fav questions to question object
+    const favAsList = createQuestionList(favoriteQuestionsList.data)
+
+    console.log('favAsList is converted', favAsList);
+
 
 
     // TODO - convert fav questions to category object
     // no id, categoryName = Favorites 
+    console.log('createCategoryListItem favAsList result:', createCategoryListItem(favAsList, 'Favorites'));
     // TODO append to allQuestionsList
 
 
     // TODO - convert custom questions to category object
+    console.log('createCategoryListItem customQuestionsList result:', createCategoryListItem(customQuestionsList, 'Custom Questions'));
     // no id, categoryName = Custom Questions
     // TODO append to allQuestionsList
     // TODO append to favoriteQuestionsList
