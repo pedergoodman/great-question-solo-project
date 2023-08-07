d421a59b1a997c0092d479caa93c5307d254457a
Peder Goodman <peder.goodman@gmail.com>
2023-08-06 19:32:30 -0500

    make createCategoryListItem helper function
    use to convert the fav list and custom question to
    the same format as the items in allQuestions

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -4,29 +4,39 @@
 // TO TEST- may need to be different for individual categories but we'll see... 
   // MAYBE - add a "category_id" property to select a specific category in questionList???
   
-export function createCategoryList(questionArray) {
-  // console.log(questionArray);
 
-  const finalArray = []
+
+export function createQuestionList(categoryListArray) {
+  
+  const questionListArray = []
     // break out categories into an array of all questions
   // combine the category id/name into each question item
-  for (const category of questionArray) {
+  for (const category of categoryListArray) {
     // separating out the category data ~{id, name}
-    const categoryData = category.category_data;
+    const categoryData = category.categoryData;
 
     // for each question, in each category, attach categoryData
-    for (const question of category.question_data) {
+    for (const question of category.questionData) {
       // merge category info and question info, push to allQuestions array
-      finalArray.push({ ...categoryData, ...question });
+      questionListArray.push({ ...categoryData, ...question });
       // console.log(allQuestions);
     };
   };
 
-  return finalArray;
+  return questionListArray;
 }
 
 
-// this doesn't do anything right now
-// export function otherTestFunction(questionArray) {
-//   console.log('inside test helper function!');
-// }
+
+export function createCategoryListItem(questionListArray, categoryName) {
+  // building an object
+  const categoryListItem = {
+    categoryData: {
+      categoryName: categoryName
+    },
+    questionData: questionListArray
+
+  }
+
+  return categoryListItem;
+}
