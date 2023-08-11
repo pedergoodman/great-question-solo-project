70fad0d5b6cf87239c21cce264026f2c4135d378
Peder Goodman <peder.goodman@gmail.com>
2023-08-11 10:30:53 -0500

    create random reducer

--- src/redux/reducers/_root.reducer.js
+++ src/redux/reducers/_root.reducer.js
@@ -3,6 +3,7 @@ import errors from './errors.reducer';
 import user from './user.reducer';
 import journals from './journal.reducer'
 import questions from './question.reducer';
+import randomCard from './random.reducer';
 
 // rootReducer is the primary reducer for our entire project
 // It bundles up all of the other reducers so our project can use them.
@@ -14,7 +15,8 @@ const rootReducer = combineReducers({
   errors, // contains registrationMessage and loginMessage
   user, // will have an id and username if someone is logged in
   journals, // contains all journals of a logged in user & a store of an active journal edit
-  questions // contains a list of all the questions a user has access to: all, favorites, custom
+  questions, // contains a list of all the questions a user has access to: all, favorites, custom
+  randomCard
 });
 
 export default rootReducer;
